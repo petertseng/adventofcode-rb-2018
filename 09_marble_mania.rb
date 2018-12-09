4a042ce4a977975a71492200813023bbd93cb6a2
Peter Tseng <pht24@cornell.edu>
2018-12-09 23:20:30 +0000

    day 09: Simulate a double-ended queue with a fixed-size array
    Runtime 3.3 seconds -> 2.1 seconds

--- 09_marble_mania.rb
+++ 09_marble_mania.rb
@@ -1,59 +1,61 @@
-Circle = Struct.new(:left, :right) do
-  def move_left
-    if left.empty?
-      self.left = right
-      self.right = []
-    end
-    right.unshift(left.pop)
-  end
-
-  def move_right
-    if right.empty?
-      self.right = left
-      self.left = []
-    end
-    left << right.shift
-  end
-
-  def insert_left(v)
-    left << v
-  end
-
-  def delete_left
-    (left.empty? ? right : left).pop
-  end
-end
-
 CYCLE = 23
 
 def play(players, n_marbles, verbose: false)
   scores = [0] * players
-  # Convention: Keep the cursor to the right of the current marble.
-  circle = Circle.new([0], [])
+
+  # Avoid resizes by allocating all space once at the beginning.
+  # The key is to look at the example and the current marble at each line.
+  # There's a relationship between the elements to the left and right of current.
+  # This solution can easily be seen as analogous to Python solutions using deque#rotate,
+  # except using a fixed-size array as the underlying storage.
+  # See ephemient's solution in Haskell.
+  marbles = [nil] * n_marbles
+  marbles[0] = 0
+
+  current = 0
+  size = 1
 
   (n_marbles / CYCLE).times { |cycle|
     base = CYCLE * cycle
 
     (CYCLE - 1).times { |i|
       marble = base + 1 + i
-      # Because we keep the cursor to the right of the current,
-      # and we insert to the left of the cursor,
-      # we only need to move right once to insert the marble
-      # and have it become the new current.
-      # Since 22/23 iterations are of this type, we want this to be fast,
-      # and doing only one move_right (instead of two) helps.
-      circle.move_right
-      circle.insert_left(marble)
+      # Insertion: current and right-of-current become left-of-current.
+      # Note that in this array, subtraction goes to the right,
+      # so that we can just use a negative index instead of doing % n_marbles.
+      # This saves on a bit of runtime.
+      marbles[current - size] = marbles[current]
+      current = (current - 1) % n_marbles
+      marbles[current - size] = marbles[current]
+      marbles[current] = marble
+      size += 1
     }
 
     marble = base + CYCLE
-    7.times { circle.move_left }
-    scores[marble % players] += marble + circle.delete_left
-    circle.move_right
+    # Deletion: Current plus five elements to the left (six in total)
+    # become right-of-current.
+    (1..6).each { |i|
+      # current + i >= marbles.size is *never* true because...?
+      # I'm not sure, no proof for this.
+      # Note that we will always have subtracted 1 from current 22 times,
+      # but what if we wrapped around in the last 6 times?
+      # For now I'll just print out a warning if I accidentally allocated.
+      # It hasn't happened in any test cases.
+      marbles[current + i] = marbles[current - size + i]
+    }
+    scores[marble % players] += marble + marbles[current - size + 7]
+    current += 6
+    size -= 1
 
-    p circle if verbose
+    if verbose
+      to_the_right = marbles[[(current - size + 1), 0].max..current]
+      remain = size - to_the_right.size
+      wrapped = marbles[-remain, remain]
+      puts "#{to_the_right.reverse} / #{wrapped.reverse} @ #{current}"
+    end
   }
 
+  puts "accidentally allocated up to #{marbles.size} up from #{n_marbles}" if marbles.size != n_marbles
   scores.max
 end
 
