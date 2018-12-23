684f0715b22465babf904a0e6abdf636d5738fca
Peter Tseng <pht24@cornell.edu>
2018-12-23 02:52:46 +0000

    day 22: Allow decrease-key on binary heap
    A modest improvement from 4.6 seconds to 3.7 seconds

--- lib/priority_queue.rb
+++ lib/priority_queue.rb
@@ -1,22 +1,29 @@
 class PriorityQueue
   def initialize
     @elts = []
+    @prio = {}
+    @idx = {}
   end
 
-  def <<(e)
-    i = @elts.size
-    until i == 0 || ((parent_v = @elts[parent_i = (i - 1) / 2]) <=> e) < 0
+  def []=(e, prio)
+    @prio[e] = prio
+    i = @idx[e] || @elts.size
+    until i == 0 || @prio[(parent_v = @elts[parent_i = (i - 1) / 2])] < prio
       @elts[i] = parent_v
+      @idx[parent_v] = i
       i = parent_i
     end
     @elts[i] = e
+    @idx[e] = i
   end
 
   def pop
     return nil if @elts.empty?
-    return @elts.pop if @elts.size == 1
-    @elts[0].tap {
-      down(0, @elts.pop)
+    last = @elts.pop
+    (@elts[0] || last).tap { |e|
+      @idx.delete(e)
+      @prio.delete(e)
+      down(0, last) unless @elts.empty?
     }
   end
 
@@ -26,18 +33,22 @@ class PriorityQueue
     while (l_v = @elts[l_i = 2 * i + 1])
       smallest_i = i
       smallest_v = v
-      if (l_v <=> smallest_v) < 0
+      smallest_prio = @prio[smallest_v]
+      if ((l_prio = @prio[l_v]) <=> smallest_prio) < 0
         smallest_i = l_i
         smallest_v = l_v
+        smallest_prio = l_prio
       end
-      if (r_v = @elts[r_i = l_i + 1]) && (r_v <=> smallest_v) < 0
+      if (r_v = @elts[r_i = l_i + 1]) && ((r_prio = @prio[r_v]) <=> smallest_prio) < 0
         smallest_i = r_i
         smallest_v = r_v
       end
       break if smallest_i == i
       @elts[i] = smallest_v
+      @idx[smallest_v] = i
       i = smallest_i
     end
     @elts[i] = v
+    @idx[v] = i
   end
 end
