72e1c4067260a04c67aa2798438f8271ad30586f
Peter Tseng <pht24@cornell.edu>
2018-12-24 12:58:01 +0000

    day 23: combinations and intersects (correct but slow)
    Interesting solution:
    https://www.reddit.com/r/adventofcode/comments/a8s17l/2018_day_23_solutions/ecdf6ci/
    
    Good for the Advent of Code inputs, where the bots involved in the
    intersection are pretty easily determined (so it's usually nCn or
    nCn-1).
    
    Runs in 2.0 seconds (not as fast as my regular 0.46 seconds).
    
    However, slow in the general case because of having to iterate
    combinations.

--- 23_emergency_teleportation.rb
+++ 23_emergency_teleportation.rb
@@ -10,6 +10,14 @@ module Nanobot refine Array do
     pos.dist(pt) <= r
   end
 
+  # Intervals is [[xmin, xmax], [ymin, ymax], [zmin, zmax]]
+  def &(intervals)
+    zip(intervals).map { |(mina, maxa), (minb, maxb)|
+      return nil if minb > maxa || mina > maxb
+      [[mina, minb].max, [maxa, maxb].min]
+    }
+  end
+
   def to4d
     x, y, z = self
     [x + y + z, x - y + z, x + y - z, x - y - z]
@@ -48,112 +56,6 @@ end end
 
 using Nanobot
 
-class Box
-  attr_reader :min, :max, :min_idx, :max_idx, :size, :point
-
-  # min, max, min_idx, max_idx are all inclusive.
-  def initialize(min, max, min_idx, max_idx)
-    @min = min.freeze
-    @max = max.freeze
-    @min_idx = min_idx.freeze
-    @max_idx = max_idx.freeze
-    @sizes = max_idx.zip(min_idx).map { |maxi, mini| maxi - mini + 1 }.freeze
-    # Just an estimate is fine.
-    @size = @sizes.reduce(1, :*)
-    @point = min.zip(max).all? { |a, b| a == b } ? min.to3d : nil
-  end
-
-  def touched_by?(bot)
-    bot.zip(@min, @max).all? { |(bmin, bmax), min, max|
-      bmin <= max && min <= bmax
-    }
-  end
-
-  def contained_by?(bot)
-    bot.zip(@min, @max).all? { |(bmin, bmax), min, max|
-      bmin <= min && min <= bmax && bmin <= max && max <= bmax
-    }
-  end
-
-  def min_dist
-    return point.sum(&:abs) if point
-
-    # This ignores the fact that there might be no 3D solution.
-    # However, this is fine since we'll discover this fact later on.
-    @min.zip(@max).map { |min, max|
-      # Opposite signs implies zero.
-      # Otherwise, the minimum absolute value (min if positive, max if negative)
-      min * max <= 0 ? 0 : min > 0 ? min : -max
-      # I beleve it's correct to take the max of the individual dimensions.
-      # Consider the 2d case:
-      # One interval may contain zero, but the other be very far away.
-      # In tha case, the minimum distance isn't zero, it's decided by the other one.
-    }.max
-  end
-
-  def split(coords)
-    size, largest_dimension = @sizes.each_with_index.max_by(&:first)
-
-    if size == 1
-      # Split into all points that have 3D equivalents.
-      ranges = @min.zip(@max).map { |min, max|
-        # Given that the intersecting region is bounded on each side by *some* bot's interval,
-        # and all this region's limits correspond to some bot's interval,
-        # I think it is always safe to only take [min, min+1, max-1, max]
-        # (+/- 1 due to needing to make sure parity matches up)
-        #
-        # However, just to be safe, we'll impose an arbitrary limit,
-        # and below the limit we'll just exhaustively check all possibilities.
-        # Arbitrarily choosing 20 because even if all four dimensions have 20,
-        # 160k will hopefully not be too bad.
-        #
-        # Note that the limit gets exceeded by the adversarial input,
-        # since the final region we split is
-        # [[-499998, 500001], [-499998, 500001], [-499998, 500001], [500000, 500001]]
-        #
-        # TODO: Three of the dimensions determine the fourth (a+d = b+c)
-        # otherwise there is no solution in 3D.
-        # Could take advantage of this to narrow down the possibilities.
-        max - min < 20 ? (min..max).to_a : [min, min + 1, max - 1, max]
-      }
-      valids = ranges[0].product(*ranges[1..-1]).select(&:to3d)
-      return valids.map { |pt| self.class.new(pt, pt, [], []) }
-    end
-
-    coord = coords[largest_dimension]
-
-    min, max = @min_idx[largest_dimension], @max_idx[largest_dimension]
-    mid = (min + max) / 2
-
-    [
-      self.class.new(
-        @min,
-        (m = @max.dup; m[largest_dimension] = coord[mid]; m),
-        @min_idx,
-        (m = @max_idx.dup; m[largest_dimension] = mid; m),
-      ),
-      self.class.new(
-        # Notice how we have to use a value that's not necessarily in coord
-        # (coord[mid] + 1 rather than coord[mid + 1])
-        # If the interval starts at an even number, but the solution needs an odd number,
-        # we can't skip ahead to coord[mid + 1] safely.
-        # See the 85761543 input for an example.
-        # We can get around this by including +/- 1 in coord,
-        # but that means each bot generates 6 partition points instead of 2,
-        # which is not worth it.
-        (m = @min.dup; m[largest_dimension] = coord[mid] + 1; m),
-        @max,
-        (m = @min_idx.dup; m[largest_dimension] = mid + 1; m),
-        @max_idx,
-      ),
-    ]
-  end
-
-  def to_s
-    @min.zip(@max).map { |min, max| min == max ? min : Range.new(min, max) }.to_s
-  end
-end
-
 surround = ARGV.delete('--surround')
 verbose = ARGV.delete('-v')
 
@@ -165,67 +67,57 @@ best_bot = bots.max_by(&:last)
 puts "best bot #{best_bot}" if verbose
 puts bots.count { |*bot, _| best_bot.cover?(bot) }
 
-# Part 2:
-# Use 4D coordinates.
-# In this way, we need only partition where intervals begin/end.
-# Thus, for N bots, for each dimension we only have 2N partition points.
-# https://old.reddit.com/r/adventofcode/comments/a9co1u/day_23_part_2_adversarial_input_for_recursive/ecmpxad/
-#
-# In 3D space, we do not have this capability,
-# because bot regions are octahedral.
-# We instead have to partition the space in half each time.
-#
-# Lower bound: Number of bots that fully contain a region
-# Upper bound: Number of bots that touch a region
-#
-# We explore areas with the best upper bounds.
-# When we explore a point (or a region where lower == upper),
-# we know the exact number of intersections there.
-
-def coords(bots)
-  [0, 1, 2, 3].map { |i| bots.flat_map { |b| b[i] }.uniq.sort }
+def common_area(bots)
+  area = [[-1.0 / 0.0, 1.0 / 0.0]] * 4
+  bots.each { |bot|
+    return nil unless area &= bot.aabb
+  }
+  area
 end
 
-def most_intersected(bots, verbose: false)
-  # Start w/ something covering all bots.
-  coords = coords(bots)
-  start = Box.new(coords.map(&:min), coords.map(&:max), [0] * 4, coords.map { |c| c.size - 1 })
-
-  puts "start w/ #{start}" if verbose
-
-  dequeues = 0
-
-  pq = PriorityQueue.new
-  # We need to order by [max upper bound, min dist]
-  # This allows us to terminate when we dequeue a point,
-  # since nothing can have a better upper bound nor be closer to the origin.
-  #
-  # Supposedly, adding size to the ordering speeds things up,
-  # but I did not observe any such effect.
-  #
-  # I was NOT convinced that adding -lower_bound to the ordering improves anything.
-  pq[start] = [-bots.size, start.min_dist, start.size]
-
-  while (region, (neg_upper_bound, _) = pq.pop(with_priority: true))
-    dequeues += 1
-    outer_upper_bound = -neg_upper_bound
-
-    if region.point
-      puts "dequeued #{region} w/ #{outer_upper_bound} bots, after #{dequeues} dequeues" if verbose
-      return region
-    end
-
-    region.split(coords).each { |split|
-      #lower_bound = bots.count { |b| split.contained_by?(b) }
-      upper_bound = bots.count { |b| split.touched_by?(b) }
-      pq[split.freeze] = [-upper_bound, split.min_dist, split.size]
-    }
-  end
+def range_to_3d(area)
+  r0, *rs = area.map { |r| Range.new(*r).to_a }
+  r0.product(*rs).map(&:to3d).compact
 end
 
-best_region = most_intersected(bots.map(&:aabb), verbose: verbose)
-best_point = best_region.point
-best_count = bots.count { |bot| bot.cover?(best_point) }
+# Count how many intersections each bot has with all others,
+# in O(n^2) time.
+intersect_counts = bots.map { |*bot1, r1|
+  bots.count { |*bot2, r2| bot1.dist(bot2) <= r1 + r2 }
+}
+
+# If we aim to find a point in range of N bots,
+# there must be at least N bots with intersect count >= N.
+intersect_freqs = intersect_counts.group_by(&:itself).transform_values(&:size)
+intersect_freqs.sort_by(&:first).reverse_each { |k, v| puts "#{k}: #{v}" } if verbose
+
+best_count = nil
+bots_with_at_least = 0
+best_point = bots.size.downto(1) { |group_size|
+  bots_with_at_least += intersect_freqs[group_size] || 0
+  next if bots_with_at_least < group_size
+
+  # This group size is eligible!
+
+  best_count = group_size
+
+  eligible_bots = bots.zip(intersect_counts).select { |bot, count|
+    count >= group_size
+  }.map(&:first)
+
+  found = []
+
+  eligible_bots.combination(group_size) { |used_bots|
+    next unless (area = common_area(used_bots))
+    found.concat(pts = range_to_3d(area))
+    puts "#{group_size} bots at #{area} - #{pts.size} points" if verbose
+  }
+
+  next if found.empty?
+
+  p found if verbose
+  break found.min_by { |pt| pt.sum(&:abs) }
+}
 
 puts "#{best_count} @ #{best_point}" if verbose
 puts best_point.sum(&:abs)
