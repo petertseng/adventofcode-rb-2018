87223de51292b464c2e8c6059a456d6b999817ae
Peter Tseng <pht24@cornell.edu>
2018-12-27 10:50:13 +0000

    crystal/search: rm priority queue and astar
    not ready to port these to crystal yet, and not needed

--- lib/search.cr
+++ lib/search.cr
@@ -1,5 +1,3 @@
-require_relative 'priority_queue'
-
 module Search
   module_function
 
@@ -10,35 +8,6 @@ module Search
       path.unshift(current)
     end
     path.freeze
-  end
-
-  def astar(start, neighbours, heuristic, goal, verbose: false)
-    g_score = Hash.new(1.0 / 0.0)
-    g_score[start] = 0
-
-    closed = {}
-    open = MonotonePriorityQueue.new
-    open[start] = heuristic[start]
-    prev = {}
-
-    while (current = open.pop)
-      next if closed[current]
-      closed[current] = true
-
-      return [g_score[current], path_of(prev, current)] if current == goal
-
-      neighbours[current].each { |neighbour, cost|
-        next if closed[neighbour]
-        tentative_g_score = g_score[current] + cost
-        next if tentative_g_score >= g_score[neighbour]
-
-        prev[neighbour] = current if verbose
-        g_score[neighbour] = tentative_g_score
-        open[neighbour] = tentative_g_score + heuristic[neighbour]
-      }
-    end
-
-    nil
   end
 
   def bfs(start, neighbours:, goal:)
