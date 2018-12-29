3ca36fb18d1c09c477a619af43b2548a954f32de
Peter Tseng <pht24@cornell.edu>
2018-12-29 21:00:19 +0000

    day 25: Only check points within 3 on the first coordinate
    Cuts runtime by about half (2 seconds -> 1 second)

--- 25_4d_adventure.rb
+++ 25_4d_adventure.rb
@@ -4,8 +4,18 @@ points = ARGF.map { |l| l.scan(/-?\d+/).map(&:to_i) }.freeze
 
 uf = UnionFind.new(points)
 
-points.combination(2) { |pt1, pt2|
-  uf.union(pt1, pt2) if pt1.zip(pt2).sum { |x, y| (x - y).abs } <= 3
+# While a plain old points.combination(2) will work,
+# let's do slightly better.
+# We only need to check points whose first coordinate differ by <= 3.
+# Cuts runtime in roughly half.
+points = points.group_by(&:first).each_value(&:freeze).freeze
+
+points.each { |c1, pts|
+  (0..3).each { |delta|
+    pts.product(points[c1 + delta] || []) { |pt1, pt2|
+      uf.union(pt1, pt2) if pt1.zip(pt2).sum { |x, y| (x - y).abs } <= 3
+    }
+  }
 }
 
 puts uf.num_sets
