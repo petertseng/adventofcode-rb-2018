b1cb9fe1eb37a7ec9ca47f4cd5a9b4a98dbac0d8
Peter Tseng <pht24@cornell.edu>
2018-12-29 21:12:31 +0000

    day 25: partition by second coordinate, in addition to first
    Cuts runtime by about half again (1 second -> 0.5 seconds)

--- 25_4d_adventure.rb
+++ 25_4d_adventure.rb
@@ -8,12 +8,21 @@ uf = UnionFind.new(input)
 # let's do slightly better.
 # We only need to check points whose first coordinate differ by <= 3.
 # Cuts runtime in roughly half.
-input = input.group_by(&:first)
+# Taking it further, we can filter by the second coordinate as well,
+# cutting runtime by half again!
+# (Filtering by a third coordinate unfortunately does not help)
+input = input.group_by { |i| i.take(2) }
 
-input.each { |c1, pts|
-  (0..3).each { |delta|
-    pts.product(input[c1 + delta] || []) { |pt1, pt2|
-      uf.union(pt1, pt2) if pt1.zip(pt2).sum { |x, y| (x - y).abs } <= 3
+input.each { |(c1, c2), pts|
+  (0..3).each { |delta1|
+    # Note that the first coordinate could look in only one direction,
+    # but this second one needs to look in both.
+    # Otherwise, we could miss points along the other diagonal.
+    d2limit = 3 - delta1
+    (-d2limit..d2limit).each { |delta2|
+      pts.product(input[[c1 + delta1, c2 + delta2]] || []) { |pt1, pt2|
+        uf.union(pt1, pt2) if pt1.zip(pt2).sum { |x, y| (x - y).abs } <= 3
+      }
     }
   }
 }
