94af1f02d5a20cafd80536c7d76cf485a0192413
Peter Tseng <pht24@cornell.edu>
2019-01-15 05:48:02 +0000

    day 01: O(n log n) solution with modular arithmetic
    This solution has been known for a long time but I thought it would be
    more trouble than it's worth. Turns out, it's really quite simple.
    
    Runtime improves from 0.15 seconds to 0.10 seconds.
    Also, can now handle inputs like "+1000000000\n-999999999"

--- 01_frequency_deltas.rb
+++ 01_frequency_deltas.rb
@@ -1,6 +1,4 @@
-require 'set'
-
-input = ARGF.map(&method(:Integer)).freeze
+input = ARGF.each_line.map(&method(:Integer)).freeze
 
 puts sum = input.sum
 
@@ -9,16 +7,19 @@ if sum == 0
   exit 0
 end
 
-# For a faster way to do part 2, see:
 # https://www.reddit.com/r/adventofcode/comments/a20646/2018_day_1_solutions/eaukxu5/
 # Summary: Track cumulative sum for one cycle through the input.
 # Note that all future iterations are offset by the total delta for one cycle.
 # The first repeated value *must* be one of the cumulative sums from the first cycle.
-
-freq = 0
-seen = Set.new
-
-puts input.cycle { |delta|
-  freq += delta
-  break freq unless seen.add?(freq)
-}
+tot_freq = 0
+cumulative_sums = input.map { |freq| tot_freq += freq }.each_with_index.to_a
+# Find the pair of numbers that are congruent to each other modulo `sum`
+# and with minimum difference between them.
+# (difference directly corresponds to number of iterations, of course)
+# In the event of a tie, earlier index in the original array wins.
+by_modulus = cumulative_sums.group_by { |x, _| x % sum }
+puts by_modulus.values.flat_map { |vals|
+  vals.sort.each_cons(2).map { |(prev_val, prev_i), (val, _)|
+    [val - prev_val, prev_i, val]
+  }
+}.min.last
