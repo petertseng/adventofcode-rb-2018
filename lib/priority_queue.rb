d166d49a71a31d252a20289d7f6244650bc6c19d
Peter Tseng <pht24@cornell.edu>
2020-01-13 11:30:03 +0000

    add monotone priority queue
    This speeds up day 22 by about 2x. Who knew it would be that easy...

--- lib/priority_queue.rb
+++ lib/priority_queue.rb
@@ -1,3 +1,24 @@
+class MonotonePriorityQueue
+  def initialize
+    @qs = []
+    @prio = 0
+    @size = 0
+  end
+
+  def []=(data, priority)
+    raise "non-monotonic add #{priority} vs #{@prio}" if priority < @prio
+    (@qs[priority] ||= []) << data
+    @size += 1
+  end
+
+  def pop
+    return nil if @size == 0
+    @prio += 1 until (q = @qs[@prio]) && !q.empty?
+    @size -= 1
+    q.pop
+  end
+end
+
 class PriorityQueue
   # http://www.cs.cmu.edu/afs/cs.cmu.edu/user/sleator/www/papers/pairing-heaps.pdf
   class PairingTree
